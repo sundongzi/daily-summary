743f83949a53d2157e0727dd29e85866877d645c
sundong <sundong@idss-cn.com>
2021-04-01 14:29:49 +0800

    依赖修改

--- docs/deploy-gh.sh
+++ docs/deploy-gh.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:<sundongzi>/<sundongzi>.github.io.git master
+git push -f https://sundongzi.github.io/dailySummary master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:sundongzi/dailySummary.git master
+# git push -f git@github.com:sundongzi/dailySummary.git master
 
 cd -
